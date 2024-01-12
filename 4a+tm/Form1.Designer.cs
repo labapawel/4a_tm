c6dd077a3c0ec32a683001856893574a9638ff23
unknown <labapawel@gmail.com>
2024-01-12 07:31:36 +0100

    start projektu

---
+++ 4a+tm/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace _4a_tm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tmButton1 = new _4a_tm.tmButton();
+            this.SuspendLayout();
+            // 
+            // tmButton1
+            // 
+            this.tmButton1.AutoSize = true;
+            this.tmButton1.Location = new System.Drawing.Point(435, 152);
+            this.tmButton1.Name = "tmButton1";
+            this.tmButton1.Size = new System.Drawing.Size(65, 226);
+            this.tmButton1.TabIndex = 0;
+            this.tmButton1.Text = "tmButton1";
+            this.tmButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tmButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private tmButton tmButton1;
+    }
+}
+
---
+++ 4a+tm/tmButton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4a_tm
+{
+    internal class tmButton : Button
+    {
+        private string pSymbol = "";
+        public string Symbol {
+                get => pSymbol;
+                set
+                {
+                    pSymbol = value;
+                    base.Text = value;
+                }
+            }
+
+        public tmButton() : base()
+        {
+            
+            this.Resize += ResizeBtt;
+        }
+
+
+        private void ResizeBtt(object sender, System.EventArgs e)
+        {
+            base.Height = base.Width;
+            base.Size = new Size(this.Width, this.Height);
+        }
+    }
+}
