3951e865fd726724c517593071c9e1b28dfac259
Pawel Laba <labapawel@gmail.com>
2024-01-19 07:54:46 +0100

    dodanie tablicy

--- 4a+tm/Form1.cs
+++ 4a+tm/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,18 +15,41 @@ namespace _4a_tm
 {
     public partial class Form1 : Form
     {
+        private string jsonstring = "";
         private string url = @"https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json";
+        private TM tm;
         public Form1()
         {
             InitializeComponent();
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    jsonstring = wc.DownloadString(url);
+                    tm = JsonConvert.DeserializeObject<TM>(jsonstring);
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            tmButton btt = new tmButton();
-            btt.Width = 80;
-            btt.Symbol = "H";
-            tableLayoutPanel1.Controls.Add(btt, 9,9);
+
+            foreach(var elem in tm.elements)
+            {
+                tmButton btt = new tmButton();
+                btt.Width = 80;
+                btt.Symbol = elem.symbol;
+                tableLayoutPanel1.Controls.Add(btt, elem.xpos, elem.ypos);
+
+            }
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
---
+++ 4a+tm/tm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4a_tm
+{
+    internal class Pierwiastek
+    {
+        public string name { get; set; }    
+        public int xpos { get; set; }   
+        public int ypos { get; set; }
+        public string symbol { get; set; }
+    }
+    internal class TM
+    {
+        public List<Pierwiastek> elements { get; set; }
+    }
+}
